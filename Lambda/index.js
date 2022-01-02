60448bc6f5202f31d18b253d853c20affbf7a962
Jitesh Srivastava <jitesh.srivastava@gmail.com>
2022-01-03 00:42:01 +0530

    Update index.js

--- Lambda/index.js
+++ Lambda/index.js
@@ -1,6 +1,6 @@
 const { v4: uuidv4} = require('uuid');
 
 exports.handler = async function (event, context, callback) {
-    const uuid = uuidv4();
+    const uuid = uuidv4(); // generator
     return 'uuid: ' + uuid;
 }
