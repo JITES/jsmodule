d3ee0067479943364839d0362df644cc931ead81
Jitesh <jitesh.srivastava@gmail.com>
2022-01-15 21:13:15 +0530

    Refactor

--- JS/generators.js
+++
@@ -1,9 +0,0 @@
-function *foo() {
-    var x = 1 + (yield "foo");
-    console.log(x);
-}
-
-let output = foo()
-console.log(output.next());
-
-
--- Other/file.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-
-class CFG {
-    static void leftRotate(int[] arr, int d, int n) {
-        
-    }
-}
